c8234714670653c6aa5efeed5df399b651343678
Nerbet <60988912+Nerbet@users.noreply.github.com>
2020-03-21 00:15:16 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function reverse (n) {
-  
+    var result = 0;
+    var digits = [];
+    var a;
+    var k =1;
+    
+     while(n != 0)
+    {
+        a = n%10;
+        n=(n-a)/10;
+        digits.push(a);
+    }
+for(var i =0; i<digits.length; i++)
+{
+   for(var j=0; j<digits.length-i-1; j++)
+   {
+       k = k*10;
+   }
+   result = result + digits[i]*k;
+   k=1;
+}
 }
